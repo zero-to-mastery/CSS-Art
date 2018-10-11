2bb52188a60c89484ebe78a5b34176ec897fdbb9
Alexander Diekmann <alexander.diekmann2@googlemail.com>
2018-10-11 15:06:30 +0200

    Please add my art to the galery

--- include.js
+++ include.js
@@ -50,6 +50,7 @@ let cards = [
     ["Arc Reactor", "./Art/AhmedH14/index.html", "./Art/AhmedH14/ScreenShot.png", "Ahmed Hajat", "https://github.com/AhmedH14"],
     ["Pixel Charizard", "./Art/mctrl/index.html", "./Art/mctrl/charizard.jpg", "Martina", "https://github.com/mctrl"],
     ["Funky Monkey", "./Art/andra/index.html", "./Art/andra/andra.png", "Andra", "https://github.com/andrapetronela"],
+    ["Kirby", ".Art/Kirby.index.html", "./Art/Kirby/Kirby.png", "Aldiekmann", "https://github.com/aldiekmann"]
 
 ];
 
