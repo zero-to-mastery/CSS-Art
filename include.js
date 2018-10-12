229398307210e7dbc73bf99792301c6b60b4363c
Redvanisation <redvanisation@gmail.com>
2018-10-12 23:14:06 +0100

    adding redvans apple

--- include.js
+++ include.js
@@ -46,6 +46,7 @@ let cards = [
     ["The Grim Reaper", "./Art/OhSnapdragon/reaper.html", "./Art/OhSnapdragon/reaper.PNG", "OhSnapdragon", "https://github.com/OhSnapdragon"],
     ["Flying Bird", "./Art/mamta/index2.html", "./Art/mamta/flying.png", "palmamta", "https://github.com/palmamta"],
     ["Creepy Selfie", "./Art/Creepy Selfie/index.html", "./Art/Creepy Selfie/CreepySelfie.png", "bronasek", "https://github.com/bronasek"],
+    ["Redvan's Apple", "./Art/redvan/index.html", "./Art/redvan/redvansApple.jpg", "Redvanisation", "https://github.com/Redvanisation"]
 ]
 
 
