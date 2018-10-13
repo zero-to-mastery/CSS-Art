4eac09e659f22e87e57d7975d6ca848224028f26
sdfnz <30536578+sdfnz@users.noreply.github.com>
2018-10-13 18:34:42 -0500

    Added my art to the collection

--- include.js
+++ include.js
@@ -55,7 +55,7 @@ let cards = [
     ["Cartoon Skull", "./Art/Jose-Rios/index.html", "./Art/Jose-Rios/skull_css.JPG", "Jose", "https://github.com/Riosjl41"],
     ["Butterfly", "./Art/R-Butterfly/index.html", "./Art/R-Butterfly/R-Butterfly.png", "Russell", "https://github.com/russellsoppe"],
     ["Robo - Dancer", "./Art/Florin/index.html", "./Art/Florin/robo-dancer.png", "Florin", "https://github.com/LoposFlorin"],
-    ["Smiley", "./Art/adrianomi/index.html", "./Art/adrianomi/smiley.jpg", "adrianomi", "https://github.com/adrianomi"],
+    ["Smiley", "./Art/adrianomi/index.html", "./Art/adrianomi/smiley.jpg", "adrianomi", "https://github.com/adrianomi"],["Beach", "./Art/sdfnz/beach.html", "./Art/sdfnz/beach.jpg", "sdfnz", "https://github.com/sdfnz"],
     ];
 
 
