c644b710c8b122166d7758eeccd778a5dffac4c2
Joy Goh-Mah <25623482+royranger@users.noreply.github.com>
2018-10-14 02:00:12 +0100

    fixes image

--- include.js
+++ include.js
@@ -56,7 +56,7 @@ let cards = [
     ["Butterfly", "./Art/R-Butterfly/index.html", "./Art/R-Butterfly/R-Butterfly.png", "Russell", "https://github.com/russellsoppe"],
     ["Robo - Dancer", "./Art/Florin/index.html", "./Art/Florin/robo-dancer.png", "Florin", "https://github.com/LoposFlorin"],
     ["Smiley", "./Art/adrianomi/index.html", "./Art/adrianomi/smiley.jpg", "adrianomi", "https://github.com/adrianomi"],
-    ["Watermelon", "./Art/watermelon/index.html", "./Art/watermelon/watermelon-screenshot.png", "Dan Horsch", "https://github.com/danhorsch"],
+    ["Watermelon", "./Art/watermelon/index.html", "./Art/watermelon/watermelon-screenshot.PNG", "Dan Horsch", "https://github.com/danhorsch"],
     ["Beach", "./Art/sdfnz/beach.html", "./Art/sdfnz/beach.jpg", "sdfnz", "https://github.com/sdfnz"],
   
   
