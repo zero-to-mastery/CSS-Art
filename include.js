734d85893a5d3e262560e5d8e2b4389eeb7ed2fb
Tősér Judit <toser.jude@gmail.com>
2018-10-22 14:40:27 +0200

    add ghost with animation

--- include.js
+++ include.js
@@ -85,7 +85,8 @@ let cards = [
     ["Space Invader", "./Art/JFox/spaceInvader.html", "./Art/JFox/spaceInvader.png", "Jeremy Fox", "https://github.com/jeremyfox36"],
    ["Beating Heart", "./Art/nikiya/index.html", "./Art/nikiya/heart-beat.png", "Nikiya Simpson", "https://github.com/nikiyasimpson"],
    ["Waving Penguin", "./Art/orinayo/index.html", "./Art/orinayo/penguin_art.png", "Orinayo Oyelade", "https://github.com/orinayo"],
-   ["Taco Animation", "./Art/roninmittal/index.html", "./Art/roninmittal/taco.png", "Ronin Mittal", "https://github.com/roninmittal"]
+   ["Taco Animation", "./Art/roninmittal/index.html", "./Art/roninmittal/taco.png", "Ronin Mittal", "https://github.com/roninmittal"],
+   ["Ghost", "./Art/toserjude/ghost.html", "./Art/toserjude/ghost.jpg", "toserjude", "https://github.com/toserjude"]
 ];
 
 
