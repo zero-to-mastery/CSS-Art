9c8c97fb6d774f9084b9e7072bb0fed53b85f0cf
Virtual1 <jessica@virtualangels.co.za>
2018-10-31 14:42:44 +0200

    Fixing Issue #170  adding commas

--- include.js
+++ include.js
@@ -70,7 +70,7 @@ let cards = [
     ["Xmas", "./Art/Chrissy/xmas.html", "./Art/Chrissy/xmas.png", "Chrissy", "https://github.com/charlecoda88"],
     ["Spooky violin", "./Art/JaniH92/index.html", "./Art/JaniH92/spookyViolin.GIF", "Jani Hänninen", "https://github.com/jh1992jh"],
     ["Responsive Bars", "./Art/Rahul/index.html", "./Art/Rahul/index.png", "Rahul", "https://github.com/rik-96"],
-	  ["Creepy Pumpkin", "./Art/Virtual1/Virtual1.html", "./Art/Virtual1/Virtual1.bmp", "Jessica Erasmus", "https://github.com/Virtual1"],
+	["Creepy Pumpkin", "./Art/Virtual1/Virtual1.html", "./Art/Virtual1/Virtual1.bmp", "Jessica Erasmus", "https://github.com/Virtual1"],
     ["Boat", "./Art/Boat/index.html", "./Art/Boat/Boat.png", "Titi Arimba", "https://github.com/titiarimba"],
     ["Koala", "./Art/Chrissy/koala.html", "./Art/Chrissy/koalaedit.png", "Chrissy", "https://github.com/charlecoda88"],
     ["Landscape", "./Art/David/index.html", "./Art/David/landscape.png", "The24thDS", "https://github.com/the24thds"],
@@ -95,8 +95,8 @@ let cards = [
     ["Baymax", "./Art/dziukers/index.html", "./Art/dziukers/Baymax.JPG", "dziukers", "https://github.com/dziukers"],
     ["Smart Phone", "./Art/chathurasam/smartphone.html", "./Art/chathurasam/screenshot.png", "Chathura Samarajeewa", "https://github.com/ChathuraSam"],
     ["Pikachu", "./Art/Sherin/pikachu.html", "./Art/Sherin/pikachu.png", "Sherin Binu", "https://github.com/SherOnDale"],
-    ["Fox", "./Art/Platy/index.html", "./Art/Platy/fox.png", "Atti", "https://github.com/Platyka"]
-    ["Just alone panda", "./Art/justpanda/index.html", "./Art/justpanda/panda.PNG", "Maximsan", "https://github.com/maximsan"]
+    ["Fox", "./Art/Platy/index.html", "./Art/Platy/fox.png", "Atti", "https://github.com/Platyka"],
+    ["Just alone panda", "./Art/justpanda/index.html", "./Art/justpanda/panda.PNG", "Maximsan", "https://github.com/maximsan"],
     ["Squirtle", "./Art/Sherin/squirtle.html", "./Art/Sherin/squirtle.png", "Sherin Binu", "https://github.com/SherOnDale"],
     ["Butterfree", "./Art/Sherin/butterfree.html", "./Art/Sherin/butterfree.png", "Sherin Binu", "https://github.com/SherOnDale"],
     ["Captain's Shield", "./Art/napvlm/shield.html", "./Art/napvlm/shield.png", "Ruslan Anisimov", "https://github.com/napvlm"],
