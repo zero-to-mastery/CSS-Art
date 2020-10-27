8eac616f1b068f8cbfda2ad0cd132671db9f1a1a
KccbzZ <thechicky21@gmail.com>
2020-10-27 16:36:34 +0000

    Adding my art

--- include.js
+++ include.js
@@ -6227,13 +6227,20 @@ let cards = [
     imageLink: "./Art/KccbzZ/cover.png",
     author: "KccbzZ",
     githubLink: "https://kccbzz.github.io/cutting-bread-loading-screen/",
-  }
+  },
   {
     artName: "ZTM_Logo",
     pageLink: "./Art/ZTM_Logo/index.html",
     imageLink: "./Art/ZTM_Logo/ZTM_logo.jpg",
     author: "Eder",
     githubLink: "https://github.com/EderIRE",
+  },
+  {
+    artName: "Melon Swing",
+    pageLink: "./Art/KccbzZ4/index.html",
+    imageLink: "./Art/KccbzZ4/cover.png",
+    author: "KccbzZ",
+    githubLink: "https://github.com/KccbzZ/melon-swing",
   }
 ];
 
