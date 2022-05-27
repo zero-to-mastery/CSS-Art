114f667730e46f29dbccb7fbb66ca277e6bd595c
Virag Kormoczy <vkormoczy@gmail.com>
2022-05-27 20:36:04 +0300

    Update include.js (#279)
    Added art name

--- include.js
+++ include.js
@@ -1,6 +1,7 @@
 let cards = [
   //  Add your card in this section
-  {
+  { 
+    artName: 'Chihuahua',
     pageLink: './Art/virag-ky/index.html',
     imageLink: './Art/virag-ky/chihuahua.png',
     author: 'Virag',
