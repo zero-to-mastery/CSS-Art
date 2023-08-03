0a84e5cef9d847e4370a793b3f0e9d2eff8d812b
Chinwenma <okoriechinwe1327@gmail.com>
2023-08-02 21:36:09 -0800

    Added Ethel_Omma

--- include.js
+++ include.js
@@ -109,6 +109,14 @@ let cards = [
     author: 'Prakhar',
     githubLink: 'https://github.com/prakharbaphna'
   },
+     {
+      artName: 'sky',
+      pageLink: './Art/Ethel_Omma/art.html',
+      imageLink: './Art/Ethel_Omma/art.gif',
+      author: 'Chinwenma',
+      githubLink: 'https://github.com/Chinwenma'
+    },
+
   {
     pageLink: './Art/Pawan/Bicycle.html',
     imageLink: './Art/Pawan/bicycle.gif',
